643168b42d25ce2e17acd99f961f28814dc0ca57
Lesia Okseniuk <hrushkachereshnia@gmail.com>
2023-04-17 01:29:56 +0300

    Practise

---
+++ src/main/java/Iterator.java
@@ -0,0 +1,14 @@
+public interface Iterator <E> {
+    boolean hasNext ();
+    E next ();
+
+    default void remove () {
+        throw new UnsupportedOperationException("remove");
+    }
+
+    /*default void forEachRemaining (Consumer<? super E> action) {
+        Objects.requireNonNull (action);
+        while (hasNext ())
+            action.accept (next());
+    }*/
+}
